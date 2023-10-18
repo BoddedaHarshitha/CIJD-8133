f98ee69f390198646e86c7224cee28aeea0ec489
BoddedaHarshitha <145585395+BoddedaHarshitha@users.noreply.github.com>
2023-10-18 12:48:33 +0530

    assignment-3

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-var parent =[React.createElement("div",{},React.createElement("h1",{},"welcome to react class")),React.createElement("p",{},
-"React lets you build user interfaces out of individual pieces called components"),React.createElement("h2",{},"React first assignment")]
+var parent=React.createElement("div",{},React.createElement("h1",{},"welcome to java script"),React.createElement("h2",{},"welcome to react"),React.createElement("h3",{},"use jsx syntax instead of java script syntax"));
 var htmlRoot=document.getElementById("root")
 var rootReact=ReactDOM.createRoot(htmlRoot)
-rootReact.render(parent)
+rootReact.render(parent);
+// using jsx syntax;
+var jav=(
+    <h1>welcome to java script
+    <h2>welcome to react 
+    <h3>use jsx syntax instead of java script syntax</h3>
+    </h2>
+    </h1>)
+var cer=document.getElementById("root")
+var reactroot=ReactDOM.createRoot(cer)
+reactroot.render(jav)
